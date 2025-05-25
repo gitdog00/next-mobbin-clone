c2dc7492d45f79d191bc3a5a72510863c2e11145
gitdog00 <gitdog00@yahoo.com>
2025-05-25 15:08:44 +0200

    Fix server component serialization error

--- components/cards-list.tsx
+++ components/cards-list.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { cn } from "@/lib/utils";
 import { CarouselCard } from "./carousels/carousel-card";
 import { CheckboxCard } from "./checkbox-card";
@@ -11,7 +13,6 @@ const GridCard = ({ children }: { children: React.ReactNode }) => (
   <div
     className={cn(
       "grid content-start [--column-gap:12px] md:[--column-gap:24px] gap-x-[--column-gap] gap-y-8 md:gap-y-10 [--min-column-width:169px] md:[--min-column-width:208px] lg:[--min-column-width:243px] [--max-column-count:7] [--total-gap-width:calc((var(--max-column-count)-1)*var(--column-gap))] [--max-column-width:calc((100%-var(--total-gap-width))/var(--max-column-count))] grid-cols-[repeat(auto-fill,minmax(max(var(--min-column-width),var(--max-column-width)),1fr))]"
-      // "max-sm:[&>*:nth-child(n+5)]:hidden max-md:[&>*:nth-child(n+7)]:hidden max-xl:[&>*:nth-child(n+9)]:hidden"
     )}
   >
     {children}
@@ -29,7 +30,7 @@ const FeatureComponent = ({ feature }: { feature: string }) => {
     case "flows":
       return <FlowsComponent />;
     default:
-      return null; // Vous pouvez également retourner un composant par défaut ou ne rien rendre.
+      return null;
   }
 };
 
